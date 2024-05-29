791798e7a69f23089fc96f4d0dcdaf84506e2987
ParitoshMittal <127023647+ParitoshMittal@users.noreply.github.com>
2024-05-29 18:43:55 +0530

    1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = r'C:\Users\parit\OneDrive\Desktop\Code\API\Images'
+UPLOAD_FOLDER = r'C:\Users\parit\OneDrive\Desktop\Code\API\Upload'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -18,6 +18,9 @@ def get_random_image():
     random_image = random.choice(images)
     return os.path.join(images_folder, random_image)
 
+# Create a list to store uploaded image paths
+uploaded_images = []
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -28,10 +31,23 @@ def upload_file():
         return jsonify({'error': 'No selected file'})
 
     if file:
-        # Simulate delay for 2 seconds
-        time.sleep(2)
-        random_image_path = get_random_image()
-        return send_file(random_image_path, mimetype='image/png')
+        # Save the uploaded file to the UPLOAD_FOLDER
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        
+        # Add the uploaded file path to the list
+        uploaded_images.append(file_path)
+        return jsonify({'message': 'File uploaded successfully'})
+
+@app.route('/recive', methods=['POST'])
+def random_image():
+    
+    # Get a random image from the 'Images' folder
+    random_image_path = get_random_image()
+
+    # Return the random image
+    return send_file(random_image_path, mimetype='image/png')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=4000)
